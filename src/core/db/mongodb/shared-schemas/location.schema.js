cf1ad7356d20560be2b7d33e6642e021b410c911
Ahmed Derbala <derbala.ahmed531992@gmail.com>
2026-04-15 16:15:05 +0100

    feat: location schema

--- src/core/db/mongodb/shared-schemas/location.schema.js
+++ src/core/db/mongodb/shared-schemas/location.schema.js
@@ -1,27 +1,30 @@
 import mongoose from 'mongoose'
 
-export const LocationSchema = new mongoose.Schema({
-	type: {
-		type: String,
-		enum: ['Point'], // only allow 'Point'
-		required: false,
-		default: 'Point'
-	},
-	coordinates: {
-		type: [Number], // [longitude, latitude]
-		required: false,
-		validate: {
-			validator: function (value) {
-				return value.length === 2
-			},
-			message: 'Coordinates must be an array of [longitude, latitude].'
+export const LocationSchema = new mongoose.Schema(
+	{
+		type: {
+			type: String,
+			enum: ['Point'], // only allow 'Point'
+			required: false,
+			default: 'Point'
+		},
+		coordinates: {
+			type: [Number], // [longitude, latitude]
+			required: false,
+			validate: {
+				validator: function (value) {
+					return value.length === 2
+				},
+				message: 'Coordinates must be an array of [longitude, latitude].'
+			}
+		},
+		sharingEnabled: {
+			type: Boolean,
+			default: false
 		}
 	},
-	sharingEnabled: {
-		type: Boolean,
-		default: false
-	}
-})
+	{ timestamps: true }
+)
 
 // Create 2dsphere index for geospatial queries
 LocationSchema.index({ coordinates: '2dsphere' })
